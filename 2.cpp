e1e51c307dbf63de30902d0052ca4f2074b1cea9
kartikpandey04 <kp4725196@gmail.com>
2025-12-03 12:20:32 +0530

    C++ function to print the Fibonacci series.

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n) {
+    int a = 0, b = 1;
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    fibonacci(n);
+    return 0;
+}
